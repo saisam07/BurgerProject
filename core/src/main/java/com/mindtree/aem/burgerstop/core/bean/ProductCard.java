844ebdec68b7567a33d141bd518b8e8f70457bde
Goutam Hegde <m1062409@mindtree.com>
2021-03-25 10:00:24 +0530

    added product card bean class

---
+++ core/src/main/java/com/mindtree/aem/burgerstop/core/bean/ProductCard.java
@@ -0,0 +1,30 @@
+package com.mindtree.aem.burgerstop.core.entity;
+
+public class ProductCard {
+	String title;
+	String description;
+	float price;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "ProductCard [title=" + title + ", description=" + description + ", price=" + price + "]";
+	}
+
+}
