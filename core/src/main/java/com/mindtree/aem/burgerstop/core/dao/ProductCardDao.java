62f98efa1a4e74004698067e75e5e666fda2f5af
Goutam Hegde <m1062409@mindtree.com>
2021-03-25 12:24:09 +0530

    added ProductCardDao class

---
+++ core/src/main/java/com/mindtree/aem/burgerstop/core/dao/ProductCardDao.java
@@ -0,0 +1,9 @@
+package com.mindtree.aem.burgerstop.core.dao;
+
+import java.util.List;
+
+import com.mindtree.aem.burgerstop.core.bean.ProductCard;
+
+public interface ProductCardDao {
+	public List<ProductCard> getData();
+}
